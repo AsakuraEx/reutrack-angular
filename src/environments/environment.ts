0d8f1b0ac0b1c9b1329cf9afebb9eb5e846fe27b
Fescobar28 <franescobar97@hotmail.com>
2025-11-25 11:30:23 -0600

    se corrige environment de producción

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiURL: 'https://api-reutrack.salud.gob.sv',
+    apiURL: 'https://api-reutrack.salud.gob.sv/api',
     appURL: 'https://reutrack.salud.gob.sv',
-    backendURL: 'http://10.168.241.44:3100'
+    backendURL: 'https://api-reutrack.salud.gob.sv'
 };
