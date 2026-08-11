5edd132b6349e5c6c59c7ac7a2685a763aa111c6
Fescobar28 <franescobar97@hotmail.com>
2026-08-11 14:55:09 -0600

    Generando environment template, eliminando entornos del seguimiento de git

---
+++ src/environments/environment-template.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: true,
+    apiURL: 'https://api.misitio.com/api',
+    appURL: 'https://misitio.com',
+    backendURL: 'https://api.misitio.com'
+};
+
+// sobrescribir como environment.ts para producción
+// sobrescribir como environment.development.ts para desarrollo local
--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: true,
-    apiURL: 'https://api-reutrack.salud.gob.sv/api',
-    appURL: 'https://reutrack.salud.gob.sv',
-    backendURL: 'https://api-reutrack.salud.gob.sv'
-};
